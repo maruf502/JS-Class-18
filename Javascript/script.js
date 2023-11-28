90b1507276a776d8e66f43a3104d2fcb41e4b3c9
Maruf Khan <marufatrai12@gmail.com>
2023-11-28 10:21:48 +0600

    JS-Class-18

---
+++ Javascript/script.js
@@ -0,0 +1,32 @@
+
+
+
+
+    // var m = prompt("Enter a First Number : ");
+    // var n = prompt("Enter a Last Number : ");
+
+    // var m1 = parseInt(m);
+    // var n2 = parseInt(n);
+
+    // var result = 0;
+
+    // for(var x = m1; x <= n2; x = x + 1){
+
+    //     var result = result + x;
+
+    // }
+
+    // document.write(result);
+
+
+
+    for(var x = 1; x <= 5; x++){
+
+        var num1 = parseInt(prompt("Enter a First Number : "));
+        var num2 = parseInt(prompt("Enter a Second Number : "));
+    
+        var res = num1 + num2;
+    
+        document.write("Result = " + res);
+
+    }
